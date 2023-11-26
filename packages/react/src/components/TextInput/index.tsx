813bb0f848d0eaf6c0a783d4a7d2a23f8d69087a
lucreiss <reis101054@gmail.com>
2023-11-26 01:35:40 -0300

    Error correction - textinput

--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -1,16 +1,14 @@
 import { ComponentProps, forwardRef, ElementRef } from "react";
 import { TextInputContainer, Prefix, Input } from "./styles";
-
-export interface TextInputProps extends ComponentProps<typeof Input>{
+export interface TextInputProps extends ComponentProps<typeof Input> {
   prefix?: string;
 }
-
 
 export const TextInput = forwardRef<ElementRef<typeof Input>, TextInputProps>(({ prefix, ...props }: TextInputProps, ref) => {
   return (
     <TextInputContainer>
       {!!prefix && <Prefix>{prefix}</Prefix>}
-      <Input type="text" {...props} />
+      <Input ref={ref} {...props} />
     </TextInputContainer>
   )
 })
